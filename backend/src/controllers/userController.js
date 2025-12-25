5e302b7a2e19f9726135d9cf93f49c7d2967b080
batuhan yıldırım <yildirimbatu52@gmail.com>
2025-12-25 16:18:44 +0300

    add get endpoint and fix gets and post endpoints

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,25 @@
 const User = require("../models/userModel");
 
+exports.getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id || isNaN(id)) {
+      return res.status(400).json({ message: "Geçersiz kullanıcı ID" });
+    }
+
+    const user = await User.getUser(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.getAllUsers();
